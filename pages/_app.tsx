bdeead029c53dcfaeb12ffd55ba302927d900deb
tobi110289 <tobias.feltes@web.de>
2021-12-29 17:58:50 +0100

    fix(loader): added css for loader animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "tailwindcss/tailwind.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { GlobalContext } from "../utility/context";
